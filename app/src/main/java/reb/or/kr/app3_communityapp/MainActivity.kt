fe8605c60657309eadc3e0be2adabf26888e734b
kimmydkemf@naver.com <kimmydkemf@gmail.com>
2022-10-24 08:04:25 +0900

    binding setting & splash window

--- app/src/main/java/reb/or/kr/app3_communityapp/MainActivity.kt
+++ app/src/main/java/reb/or/kr/app3_communityapp/MainActivity.kt
@@ -2,10 +2,21 @@ package reb.or.kr.app3_communityapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import reb.or.kr.app3_communityapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        SystemClock.sleep(1000)
+        setTheme(R.style.Theme_App3_CommunityApp)
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
